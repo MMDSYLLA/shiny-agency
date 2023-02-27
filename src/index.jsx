52dafec3a7499b275adb6db3ee0155fd9b3775e9
Sylla <mmdsylla@gmail.com>
2023-02-27 19:27:59 +0100

    #3 modif de src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,48 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import Survey from './pages/survey/survey-api'
-
-import Results from './pages/Results'
-import Freelances from './pages/Freelances'
+import Survey from './pages/survey/index1'
 import Header from './components/Header'
-import Error from './components/Error'
-import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-
-    body {
-      margin: 0;
-    }
-`
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <GlobalStyle />
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/survey/:questionNumber">
-          <Survey />
-        </Route>
-        <Route path="/results">
-          <Results />
-        </Route>
-        <Route path="/freelances">
-          <Freelances />
-        </Route>
-        <Route>
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            
+            <Header />
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/survey/:questionNumber">
+                <Survey />
+            </Route>
+
+        </Router>
+    </React.StrictMode>,
+document.getElementById('root')
 )
